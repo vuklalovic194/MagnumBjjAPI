612012cb632bc610432f389f26a28a612f748c00
Vuk <vuklalovic194@gmail.com>
2023-11-09 11:24:20 +0100

    C:/Program Files/Git/

---
+++ Magnum.API.Unittest/CreateMemberCommandHandlerTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Magnum.API.Unittest.Members.Command
+{
+	public class CreateMemberCommandHandlerTest
+	{
+		[Fact]
+		public void Handle_Should_ReturnFailure_WhenNameIsNotUnique()
+		{
+			//Arange
+
+			var command = new CreateMemberCommand();
+
+			//Act
+			
+			//Assert
+		}
+	}
+}
